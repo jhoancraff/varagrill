a8178d284cb4bc9ab9c30020d332c1ad1572b0ef
Jhoan Gonzalez <jhoangonzalezcraff@gmail.com>
2026-08-24 18:12:28 -0400

    Arreglado el bug de las listas

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { registerSW } from 'virtual:pwa-register';
+import './index.css';
 import App from './App';
 
 const updateSW = registerSW({
